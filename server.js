91a8ff32fdd7f2e492669d31905a3db4c812b4ed
Josserand Yole <joss.yole@gmail.com>
2026-06-26 17:42:11 +0200

    Correction CORS Netlify

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.use(compression());
 app.use(
   cors({
     origin: [
+      "https://adminfacile-app.netlify.app",
       "https://venerable-pixie-e9c9a9.netlify.app",
       "http://localhost:5500",
       "http://127.0.0.1:5500",
